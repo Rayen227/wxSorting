e7b975ab25e928927b46f05a32725cacfb8dd763
user_DE <16607615570@163.com>
2021-02-21 14:55:07 +0800

    添加动作及游戏结束逻辑

--- assets/Script/Game.js
+++ assets/Script/Game.js
@@ -13,6 +13,7 @@ cc.Class({
             type: cc.Node
         },
         rubishAtlas: cc.SpriteAtlas,
+        gameOverTesting: true,
 
     },
     // LIFE-CYCLE CALLBACKS:
@@ -23,6 +24,13 @@ cc.Class({
         //生成垃圾的速率(ms)
         this.rate = 1000;
         this.score = 0;
+        this.playing = true;
+        this.rubishX0 = 93.75;
+        this.rubishY0 = 1200;
+        this.rubishXInte = 187.5;
+
+        this.rubishStack = new Array();
+
         //score组件
         this.scoreLabel = cc.find('Canvas/BaseView/Score').getComponent(cc.Label);
 
@@ -30,35 +38,109 @@ cc.Class({
         this.scoreLabel._string = "0";
         //生成垃圾
         this.spawnARubish();
-        setInterval(function () {
+        this.genarating = setInterval(function () {
             then.spawnARubish();
         }, this.rate);
 
 
     },
+    gameOver() {
+
+        if (this.gameOverTesting) return;
+
+        //清除生成函数
+        clearInterval(this.genarating);
+        //暂停所有动作
+        for (var i = 0; i < this.rubishStack.length; i++) {
+            this.rubishStack[i].node.stopAllActions();
+        }
+    },
+
+    //生成一个垃圾节点并添加至场景
     spawnARubish() {
+
         //生成节点
         var rubishNode = cc.instantiate(this.rubishPrefab);
+
         //获取Sprite组件
-        var rubishSprite = rubishNode.getComponent(cc.Sprite);
+        var rubishSprite = rubishNode.children[0].getComponent(cc.Sprite);
+
         //获取prefab组件
         var Rubish = rubishNode.getComponent("Rubish");
+        //其他参数/变量
+        var touchStart;
+        var touchEnd;
+        var then = this;
+        console.log(rubishNode.children);
 
-
-        //随机生成垃圾资源所在id
+        //随机生成垃圾的参数
         Rubish.id = Math.floor(Math.random() * 10);
+        Rubish.type = Math.floor(Math.random() * 4);
+        Rubish.channel = Math.floor(Math.random() * 4);
+
+        //垃圾其他参数初始化
+        Rubish.speed = 200;
+        Rubish.node.x = this.rubishX0 + Rubish.channel * this.rubishXInte;
+        Rubish.node.y = this.rubishY0;
+
+        //设置动画
+        // Rubish.node.runAction(cc.moveBy(1200 / Rubish.speed, cc.v2(0, -1200)));
+        rubishNode.runAction(cc.moveBy(1200 / Rubish.speed, cc.v2(0, -1200)));
+
+        //注册事件
+
+
+        Rubish.node.on(cc.Node.EventType.TOUCH_START, function (e) {//开始触摸
+            touchStart = e.getLocation();
+        });
+
+        function touchEndAction(e) {
+            touchEnd = e.getLocation();
+            if (touchEnd.x - touchStart.x > 0 && Rubish.channel != 3) {//向右移
+                Rubish.channel++;
+                Rubish.node.runAction(
+                    cc.moveBy(0.2, cc.v2(187.5, 0)).easing(cc.easeCubicActionOut())
+                );
+            } else if (touchEnd.x - touchStart.x < 0 && Rubish.channel != 0) {//向左移
+                Rubish.channel--;
+                Rubish.node.runAction(
+                    cc.moveBy(0.2, cc.v2(-187.5, 0)).easing(cc.easeCubicActionOut())
+                );
+            }
+            console.log("Current channel:", Rubish.channel);
+        }
+        Rubish.node.on(cc.Node.EventType.TOUCH_CANCEL, touchEndAction);
+        Rubish.node.on(cc.Node.EventType.TOUCH_END, touchEndAction);
+
+
+        Rubish.update = function () {
+            if (Rubish.node.position.y <= 175) {
+                if (Rubish.type == Rubish.channel) {
+                    console.log("匹配成功");
+                    then.score++;
+                    then.scoreLabel.string = then.score.toString();
+                } else {
+                    console.log("匹配失败");
+                    then.gameOver();
+                }
+                Rubish.node.destroy();
+                Rubish = null;
+                //出栈
+                then.rubishStack.shift();
+            }
+        };
 
         //根据id设置图片资源
         rubishSprite.spriteFrame = this.rubishAtlas.getSpriteFrame(Rubish.type + '_' + Rubish.id);
-
+        // console.log(Rubish.type + '_' + Rubish.id);
 
         this.node.addChild(rubishNode);
-        console.log(Rubish.type, Rubish.channel);
+        //加入暂存栈
+        this.rubishStack.push(Rubish);
+        // this.rubishPool.put(Rubish);
+        // console.log(Rubish.type, Rubish.channel);
     },
 
     update(dt) {
-        // this.scoreLabel._string = this.score.toString();
-        // console.log(this.score.toString());
-        // console.log(this.score, this.scoreLabel._string);
     },
 });
--- assets/Script/Rubish.js
+++ assets/Script/Rubish.js
@@ -2,77 +2,74 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        type: 0,//垃圾的类型
-        channel: 0,//垃圾现在所处的轨道
-        speed: 0,
-        id: 0,//垃圾的图片下标
         rubishAtlas: cc.SpriteAtlas,
-        parent: {
-            default: null,
-            type: cc.Node
-        }
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad() {
+
         // v_0 = 200pix/s
-        this.speed = 200;
-        this.type = Math.floor(Math.random() * 4);
-        this.channel = Math.floor(Math.random() * 4);
-        this.node.runAction(this.setFallAction());
-        this.x0 = 93.75;
-        this.xInte = 187.5;
-        this.y0 = 1200;
-        this.game = cc.find('Canvas/BaseView').getComponent("Game");
-        var then = this;
-        console.log(this.game.scoreLabel);
-        this.node.x = this.x0 + this.channel * this.xInte;
-        this.node.y = this.y0;
-        var touchStart;
-        var touchEnd;
+        // this.speed = 200;
+        // this.type = Math.floor(Math.random() * 4);
+        // this.channel = Math.floor(Math.random() * 4);
+        // this.node.runAction(this.setFallAction());
+        // this.x0 = 93.75;
+        // this.xInte = 187.5;
+        // this.y0 = 1200;
+        // this.game = cc.find('Canvas/BaseView').getComponent("Game");
+        // var then = this;
+        // this.node.x = this.x0 + this.channel * this.xInte;
+        // this.node.y = this.y0;
+        // var touchStart;
+        // var touchEnd;
+
+
+        // console.log("type:", this.type);
         //注册触摸事件
-        this.node.on(cc.Node.EventType.TOUCH_START, function (e) {//开始触摸
-            touchStart = e.getLocation();
-        });
-        this.node.on(cc.Node.EventType.TOUCH_CANCEL, function (e) {//触摸结束
-            touchEnd = e.getLocation();
-            if (touchEnd.x - touchStart.x > 0 && then.channel != 3) {//向右移
-                console.log("向右移");
-                then.channel++;
-                then.node.runAction(then.toRightAction());
-            } else if (touchEnd.x - touchStart.x < 0 && then.channel != 0) {
-                console.log("向左移");
-                then.channel--;
-                then.node.runAction(then.toLeftAction());
-            }
-            console.log("Current channel:", then.channel);
-        });
+        // this.node.on(cc.Node.EventType.TOUCH_START, function (e) {//开始触摸
+        //     touchStart = e.getLocation();
+        // });
+        // this.node.on(cc.Node.EventType.TOUCH_CANCEL, function (e) {//触摸结束
+        //     touchEnd = e.getLocation();
+        //     if (touchEnd.x - touchStart.x > 0 && then.channel != 3) {//向右移
+        //         console.log("向右移");
+        //         then.channel++;
+        //         then.node.runAction(then.toRightAction());
+        //     } else if (touchEnd.x - touchStart.x < 0 && then.channel != 0) {
+        //         console.log("向左移");
+        //         then.channel--;
+        //         then.node.runAction(then.toLeftAction());
+        //     }
+        //     console.log("Current channel:", then.channel);
+        // });
 
     },
+    // getType() {
+    //     return this.type;
+    // },
+    // setFallAction() {
+    //     return cc.moveBy(1200 / this.speed, cc.v2(0, -1200));
+    // },
 
-    setFallAction() {
-        return cc.moveBy(1200 / this.speed, cc.v2(0, -1200));
-    },
-
-    toRightAction() {
-        return cc.moveBy(0.2, cc.v2(187.5, 0)).easing(cc.easeCubicActionOut());
-    },
-    toLeftAction() {
-        return cc.moveBy(0.2, cc.v2(-187.5, 0)).easing(cc.easeCubicActionOut());
-    },
+    // toRightAction() {
+    //     return cc.moveBy(0.2, cc.v2(187.5, 0)).easing(cc.easeCubicActionOut());
+    // },
+    // toLeftAction() {
+    //     return cc.moveBy(0.2, cc.v2(-187.5, 0)).easing(cc.easeCubicActionOut());
+    // },
     update() {
 
-        if (this.node.position.y <= 175) {
-            if (this.type == this.channel) {
-                console.log("匹配成功");
-                this.game.score++;
-                this.game.scoreLabel.string = this.game.score.toString();
-            } else {
-                console.log("匹配失败");
-            }
-            this.node.destroy();
-        }
+        // if (this.node.position.y <= 175) {
+        //     if (this.type == this.channel) {
+        //         // console.log("匹配成功");
+        //         this.game.score++;
+        //         this.game.scoreLabel.string = this.game.score.toString();
+        //     } else {
+        //         // console.log("匹配失败");
+        //     }
+        //     this.node.destroy();
+        // }
 
     }
 
